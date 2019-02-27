031c4e0223b73c5d74c0a2388d79b8d7a4eab0fb
favorable-vector-1743 <epzads@gmail.com>
2019-02-27 20:14:50 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,25 +1,27 @@
 def division(num1, num2)
- 
+ return (num1/num2)
 end
 
 def assign_variable(value)
-  
+  name = value 
+  return name 
 end
 
-def argue
-  
+def argue(argument)
+  return argument
 end
 
-def greeting
+def greeting(greet1,greet2)
+return  greet1, greet2
 
 end
 
-def return_a_value
-
+def return_a_value()
+return "nice"
 end
 
 def last_evaluated_value
-
+return 
 end
 
 def pizza_party
